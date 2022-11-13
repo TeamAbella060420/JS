aa7551afe9ca372f9266169aa3ef93563accfb22
TeamAbella060420 <jabella@rscm.com.sa>
2022-11-13 10:36:30 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,19 @@
+// console.log('Hello')
+// console.warn('Hello warning')
+// console.error('Hello error')
+
+// let Name = "Pedro";
+// let Name = "Anna";
+// console.log(Name);
+
+// data types
+//string, numbers, Boolean, null, undefined
+
+const Name = 'Pedro';
+const Age = 19;
+const Rating = 49.3;
+const inComplete = true;
+const Val = null;
+const unk = undefined;
+
+console.log(Name.toLocaleUpperCase());
